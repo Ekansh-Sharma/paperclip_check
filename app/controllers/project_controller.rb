c967dbbbf56330f1c95672a27ca9c9aaf4ba8ed1
Ekanshsharma@18 <ekanshsharmacse11@gmail.com>
2016-09-01 09:26:31 +0530

    Add image field in project model to show image.

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,7 +1,24 @@
 class ProjectController < ApplicationController
   def index
+    @projects = Project.all
   end
 
-  def show
+  def new
+    @project = Project.new
   end
+
+  def show 
+  end
+
+  def create
+    @project = Project.create( project_params )
+    redirect_to project_index_path
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:image)
+  end
+
 end
